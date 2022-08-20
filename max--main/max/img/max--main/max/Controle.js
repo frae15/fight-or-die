dd90d414671693ec653aed3dd9f93d3a62eb0202
frae15 <103329117+frae15@users.noreply.github.com>
2022-08-19 22:53:44 -0300

    oi

---
+++ max--main/max/img/max--main/max/Controle.js
@@ -0,0 +1,73 @@
+class Controle {
+    constructor() {
+        this.w = false;
+        this.s = false;
+        this.d = false;
+        this.a = false;
+        this.dirX = "d";
+    }
+
+    changeDir() {
+        this.dirX = this.dirX == "d" ? "e" : "d";
+    }
+
+    toggle(tecla) {
+        return this[tecla] = !this[tecla];
+    }
+
+    enable(tecla) {
+        this[tecla] = true;
+    }
+
+    disable(tecla) {
+        this[tecla] = false;
+    }
+
+    enableW() {
+        this.enable("w");
+    }
+
+    enableS() {
+        this.enable("s");
+    }
+
+    enableA() {
+        this.enable("a");
+    }
+
+    enableD() {
+        this.enable("d");
+    }
+
+    disableW() {
+        this.disable("w");
+    }
+
+    disableS() {
+        this.disable("s");
+    }
+
+    disableA() {
+        this.disable("a");
+    }
+
+    disableD() {
+        this.disable("d");
+    }
+
+    toggleW() {
+        this.toggle("w");
+    }
+
+    toggleS() {
+        this.toggle("s");
+    }
+
+    toggleA() {
+        this.toggle("a");
+    }
+
+    toggleW() {
+        this.toggle("d");
+    }
+}
